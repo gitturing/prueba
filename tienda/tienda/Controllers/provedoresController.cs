e822c1b51f4edaffa32a5febbf5d0e9d6bbb5061
gitturing <fcuadros54@gmail.com>
2018-06-15 21:39:26 -0500

    vistas provedores

---
+++ tienda/tienda/Controllers/provedoresController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using tienda.Models;
+
+namespace tienda.Controllers
+{
+    public class provedoresController : Controller
+    {
+        private tiendaEntities db = new tiendaEntities();
+
+        // GET: provedores
+        public ActionResult Index()
+        {
+            return View(db.provedores.ToList());
+        }
+
+        // GET: provedores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            provedores provedores = db.provedores.Find(id);
+            if (provedores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(provedores);
+        }
+
+        // GET: provedores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: provedores/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_provedor,nombre_provedor,direccion,telefono,correo")] provedores provedores)
+        {
+            if (ModelState.IsValid)
+            {
+                db.provedores.Add(provedores);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(provedores);
+        }
+
+        // GET: provedores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            provedores provedores = db.provedores.Find(id);
+            if (provedores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(provedores);
+        }
+
+        // POST: provedores/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_provedor,nombre_provedor,direccion,telefono,correo")] provedores provedores)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(provedores).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(provedores);
+        }
+
+        // GET: provedores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            provedores provedores = db.provedores.Find(id);
+            if (provedores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(provedores);
+        }
+
+        // POST: provedores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            provedores provedores = db.provedores.Find(id);
+            db.provedores.Remove(provedores);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
